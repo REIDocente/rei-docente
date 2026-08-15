5ca7b32da07c1c5cfc6403d07f9e6ab5362b326f
Jacque <jvaliente@corporacioncolina.cl>
2026-08-15 18:56:06 -0400

    fix: agregar OAs faltantes 1B-4B (OA 15,16,17,21 y equiv.) a staticCurriculum

--- commit_rei.bat
+++ commit_rei.bat
@@ -3,7 +3,7 @@ cd /d "C:\Users\56940\Desktop\rei-docente"
 del /f /q ".git\HEAD.lock" 2>nul
 del /f /q ".git\index.lock" 2>nul
 git add -A
-git commit -m "feat: agregar OAs 1B-4B a staticCurriculum + fix filtros indicadores mediano plazo"
+git commit -m "fix: agregar OAs faltantes 1B-4B (OA 15,16,17,21 y equiv.) a staticCurriculum"
 git pull --rebase
 git push
 echo.
